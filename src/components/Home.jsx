b8c91520e9fbdc77cf2f4f0292e4ac1d2e113441
Gabriel Azevedo <azevgabriel@gmail.com>
2021-03-06 14:01:34 -0300

    Content and Images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,17 @@ const Home = () => {
     
     return(
         <div className={styles.container}>
-    
+            <h1>Bem vindo,</h1>
+            <p>esse é meu currículo virtual, aqui você encontrará várias informações sobre o meu perfil.</p>
+            <br/>
+            <p>Sumário para barra de navegação:</p>
+            <br/>
+            <p><strong>- Sobre:</strong> Objetivos, habilidade em linguagens e bibliotecas.</p>
+            <p><strong>- Projetos:</strong> Projetos, descrição sobre eles e linguagens utilizadas.</p>
+            <p><strong>- Formação:</strong>: Formação acadêmica, cursos e afins.</p>
+            <p><strong>- Contato:</strong> Para enviar uma mensagem direta para meu e-mail.</p>
+            <br/>
+            <p>Além da barra de navegação, os dois ícones de redes sociais são botões para redirecionamento  direto para as respectivas redess.</p>
         </div>
     );
 }
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -4,8 +4,96 @@ const Sobre = () => {
     
     return(
         <div className={styles.container}>
-            <div>
-
+            <div className={styles.containerGoals}>
+                <h3>✔ Objetivo ✔</h3>
+                <div><input type="checkbox" checked/><label>Primeira aplicação própria;</label></div> 
+                <div><input type="checkbox" disabled/><label>Inglês Intermediário;</label></div> 
+                <div><input type="checkbox" disabled/><label>Estágio em desenvolvimento;</label></div> 
+                <div><input type="checkbox" disabled/><label>Reconhecimento na minha área;</label></div> 
+            </div>
+            <div className={styles.containerLanguages}>
+                <h3>✔ Linguagens de Programação ✔</h3>
+                <div>
+                    <label>HTML</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" checked/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>CSS</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" checked/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>JS</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" checked/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>C</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" checked/><label>Noob</label></div> 
+                    <div><input type="checkbox" disabled/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>Assembly</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" checked/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>PHP</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" checked/><label>Noob</label></div> 
+                    <div><input type="checkbox" disabled/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>Java</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" checked/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" disabled/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <h3>✔ Bibliotecas ✔</h3>
+                <div>
+                    <label>ReactJS</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" checked/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>Node.JS</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" disabled/><label>Desejável</label></div> 
+                    <div><input type="checkbox" checked/><label>Noob</label></div> 
+                    <div><input type="checkbox" disabled/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div>
+                <div>
+                    <label>Laravel</label>
+                    <div className={styles.containerLanguagesLevels}>
+                    <div><input type="checkbox" checked/><label>Desejável</label></div> 
+                    <div><input type="checkbox" disabled/><label>Noob</label></div> 
+                    <div><input type="checkbox" disabled/><label>Intermediário ++</label></div> 
+                    </div> 
+                </div> 
             </div>
         </div>
     );
