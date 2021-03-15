07a5c4c215cc0d608ee504e104204f64ebbf98c3
Gabriel Azevedo <azevgabriel@gmail.com>
2021-03-15 12:20:05 -0300

    Tablet Respondive and Contato FrontEnd

--- src/components/Bar/ContatoBar.jsx
+++ src/components/Bar/ContatoBar.jsx
@@ -3,7 +3,10 @@ import styles from '../../styles/components/Bar/ContatoBar.module.css'
 export const ContatoBar = () => {
     return(
         <div className={styles.container}>
-            Hello World!
+            <div>
+                <a href="https://www.linkedin.com/in/azevgabriel/"><img src="svg/linkedin-logo.svg"/></a>
+                <a href="https://github.com/azevgabriel"><img src="svg/github.svg"/></a>
+            </div>
         </div>  
     );
 }
--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,12 +1,47 @@
+import { useState } from 'react';
 import styles from '../styles/components/Contato.module.css'
 
 const Contato = () => {
-    
+        
+    const [campos, setCampos] = useState({
+        nome: '',
+        email: '',
+        mensagem: '',
+        anexo: ''
+    });
+
+    function handleInputChange(event){
+        if(event.target.name === "anexo")
+        campos[event.target.name] = event.target.files[0];
+        else
+        campos[event.target.name] = event.target.value;
+        setCampos(campos);
+    }
+
+    function handleFormSubmit(event){
+        event.preventDefault();
+        console.log(campos);
+    }
+
     return(
         <div className={styles.container}>
-            <div>
-
-            </div>
+            <form className={styles.forms}>
+                <div>
+                    <label htmlFor="email">E-mail:</label>
+                    <input type="text" id="email" name="email" value="azevgabriel@gmail.com" onChange={handleInputChange}/>
+                </div>
+                <div>
+                    <label htmlFor="nome">Nome:</label>
+                    <input type="text" id="nome" name="nome" placeholder="Nome da pessoa..." onChange={handleInputChange} />
+                </div>
+                <label htmlFor="mensagem">Mensagem:</label>
+                <textarea id="mensagem" name="mensagem" placeholder="Escreva algo..." className="textArea" onChange={handleInputChange}></textarea>
+                <div>
+                    <label htmlFor="anexo">Anexo:</label>
+                    <input type="file" id="anexo" name="anexo" />
+                </div>
+                <input type="submit" value="Enviar" onSubmit={handleFormSubmit}/>     
+             </form>
         </div>
     );
 }
